6978bab543ce3ff03cb8560043a18084e3209c73
aylamar <aylamar@users.noreply.github.com>
2022-06-02 18:47:25 -0500

    fix: allow svelte to render pages

--- src/server/routes/route.controller.ts
+++ src/server/routes/route.controller.ts
@@ -4,8 +4,4 @@ import apiRoutes from './api/api.controller.js';
 let router = Router();
 router.use('/api', apiRoutes);
 
-router.get('/', async (req, res) => {
-    await res.render('index', {layout: false, title: 'Home'});
-});
-
 export default router;
