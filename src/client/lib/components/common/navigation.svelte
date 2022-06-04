f27051b5c412061ba25e557cdf92690d16d53673
aylamar <aylamar@users.noreply.github.com>
2022-06-03 21:07:00 -0500

    feat: add navigation

---
+++ src/client/lib/components/common/navigation.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<header class="p-4">
+    <nav>
+        <a class="text-3xl font-bold" href="/">Lamarbooru</a>
+        <div class="sm:flex space-x-3">
+            <a class="" href="/posts">Posts</a>
+            <a class="" href="/uploads/new">Upload</a>
+        </div>
+    </nav>
+</header>
--- src/client/routes/__layout.svelte
+++ src/client/routes/__layout.svelte
@@ -1,6 +1,21 @@
-<script>
-    import "$lib/app.css";
+<script context="module" lang="ts">
+    import { currUrl } from "$lib/stores/general";
+    export async function load({url}) {
+        currUrl.set(url.pathname)
+        return {
+            status: 200
+        }
+    }
 </script>
+
+<script lang="ts">
+    import '$lib/app.css'
+    import Navigation from "$lib/components/common/navigation.svelte";
+</script>
+
+{#if $currUrl !== "/"}
+    <Navigation />
+{/if}
 
 <main class="p-4 container-1xl mx-auto">
     <slot></slot>
