15a78bae98fded5ca35b8bbca1fff95a93c701dd
aylamar <aylamar@users.noreply.github.com>
2022-06-18 17:18:17 -0500

    feat: greatly improve subscription styling

---
+++ src/client/lib/utils/subscription.ts
@@ -0,0 +1,17 @@
+export function getStyle(rowNumber: number) {
+    if (rowNumber % 2 === 0) {
+        return 'bg-slate-800/50';
+    }
+    return 'bg-slate-800/25';
+}
+
+export function getStatusStyle(status: string) {
+    switch (status) {
+        case 'running':
+            return 'p-1 text-xs uppercase text-green-700 bg-green-200 rounded-md';
+        case 'paused':
+            return 'p-1 text-xs uppercase text-slate-700 bg-slate-200 rounded-md';
+        case 'waiting':
+            return 'p-1 text-xs uppercase text-purple-700 bg-purple-200 rounded-md';
+    }
+}
--- src/client/routes/subscriptions/index.svelte
+++ src/client/routes/subscriptions/index.svelte
@@ -3,6 +3,9 @@
     import { hostname } from '../../lib/stores/general';
     import { subscriptions } from '../../lib/stores/subscriptions';
     import { callAPI } from '../../lib/utils/api';
+    import { formatDate} from '../../lib/utils/formatting.js';
+    import { getStyle } from '../../lib/utils/subscription';
+    import { getStatusStyle } from "../../lib/utils/subscription.js";
 
     onMount(async () => {
         await callAPI({
@@ -14,76 +17,42 @@
             },
         });
     });
-
-    const formatDate = (date) => {
-        let d = new Date(date),
-            month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear();
-        if (month.length < 2)
-            month = '0' + month;
-        if (day.length < 2)
-            day = '0' + day;
-        return [year, month, day].join('/');
-    };
 </script>
-<div class="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25">
+<div class="not-prose relative rounded-xl bg-slate-800/25">
     {#if $subscriptions.length >= 1}
-        <div class="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>
-        <div class="relative rounded-xl overflow-auto">
-            <div class="shadow-sm overflow-hidden my-8">
-                <table class="border-collapse w-full text-md">
-                    <thead>
+<!--        <div class="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>-->
+        <div class="relative rounded-xl">
+            <div class="shadow-sm my-8">
+                <table class="text-md w-full">
+<!--                    <caption class="text-xl font-bold text-center">Subscriptions</caption>-->
+                    <thead class="border-b border-slate-700">
                     <tr>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            ID
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            Site
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            Tags
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            Blacklisted Tags
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            Status
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            Interval
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            Next Run
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                            Total Runs
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            Image Limit
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            Create Date
-                        </th>
-                        <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 pb-3 dark:text-slate-200 text-left">
-                            More Details
-                        </th>
+                        <th class="p-3 w-16 font-medium text-slate-200 tracking-wide text-left">ID</th>
+                        <th class="p-3 w-24 font-medium text-slate-200 tracking-wide text-left">Site</th>
+                        <th class="p-3 font-medium text-slate-200 tracking-wide text-left">Tags</th>
+                        <th class="p-3 font-medium text-slate-200 tracking-wide text-right">Blacklisted Tags</th>
+                        <th class="p-3 w-20 font-medium text-slate-200 tracking-wide text-right">Status</th>
+                        <th class="p-3 w-20 font-medium text-slate-200 tracking-wide text-right">Interval</th>
+                        <th class="p-3 w-38 font-medium text-slate-200 tracking-wide text-right">Next Run</th>
+                        <th class="p-3 w-28 font-medium text-slate-200 tracking-wide text-right">Total Runs</th>
+                        <th class="p-3 w-32 font-medium text-slate-200 tracking-wide text-right">More Details</th>
                     </tr>
                     </thead>
-                    {#each $subscriptions as sub}
-                        <tr>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{sub.id}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{sub.site}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{sub.tags}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{sub.tagBlacklist.join(' ') || 'None'}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{sub.status}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{sub.interval}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{formatDate(sub.nextRun)}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 text-slate-500 dark:text-slate-400">{sub._count.runs}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{sub.limit}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400">{formatDate(sub.createDate)}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-3 pl-8 dark:text-slate-400"><a
-                                    href={`/subscriptions/${sub.id}`}>More Details</a></td>
+                    {#each $subscriptions as sub, i}
+                        <tr class={getStyle(i)}>
+                            <td class="p-3 text-slate-400">{sub.id}</td>
+                            <td class="p-3 text-slate-400">{sub.site}</td>
+                            <td class="p-3 text-slate-400">{sub.tags}</td>
+                            <td class="p-3 text-slate-400 text-right">{sub.tagBlacklist.join(' ') || 'none'}</td>
+                            <td class="p-3 text-slate-400 text-right">
+                                <span class={getStatusStyle(sub.status)}>{sub.status}</span>
+                            </td>
+                            <td class="p-3 text-slate-400 text-right">{sub.interval}</td>
+                            <td class="p-3 text-slate-400 text-right">{formatDate(sub.nextRun)}</td>
+                            <td class="p-3 text-slate-400 text-right">{sub._count.runs}</td>
+                            <td class="p-3 text-slate-400 text-right">
+                                <a href={`/subscriptions/${sub.id}`}>More Details</a>
+                            </td>
                         </tr>
                     {/each}
                 </table>
