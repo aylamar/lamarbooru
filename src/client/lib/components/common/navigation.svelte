8758905cf5e0f4f34522f4477af08ed8222a3ccc
aylamar <aylamar@users.noreply.github.com>
2022-06-18 21:52:13 -0500

    feat: add subscriptions to mini-navigation

--- src/client/lib/components/common/navigation.svelte
+++ src/client/lib/components/common/navigation.svelte
@@ -63,16 +63,28 @@
             {:else}
                 <a class="px-2" href="/files">Files</a>
             {/if}
-            <a class="px-2" href="/subscriptions">Subscriptions</a>
+            {#if $page.url.pathname.includes('/subscriptions')}
+                <a class="bg-slate-800/75 px-2" href="/subscriptions">Subscriptions</a>
+            {:else}
+                <a class="px-2" href="/subscriptions">Subscriptions</a>
+            {/if}
             <a class="px-2" href="/upload">Upload</a>
         </div>
         {#if $page.url.pathname.includes('/files')}
-            <div class="bg-slate-800 pl-2 py-0.5">
-                <div class="pl-4 sm:flex space-x-6 bg-slate-800">
+            <div class="bg-slate-800/50 pl-2 py-0.5">
+                <div class="pl-4 sm:flex space-x-6">
                     <span class="hover:cursor-pointer" on:click={handleFileClick()}>Files</span>
                     <span class="hover:cursor-pointer" on:click={handleFileClick('archived')}>Archive</span>
                     <span class="hover:cursor-pointer" on:click={handleFileClick('inbox')}>Inbox</span>
                     <span class="hover:cursor-pointer" on:click={handleFileClick('trash')}>Trash</span>
+                </div>
+            </div>
+        {/if}
+        {#if $page.url.pathname.includes('/subscriptions')}
+            <div class="bg-slate-800/50 pl-2 py-0.5">
+                <div class="pl-4 sm:flex space-x-6">
+                    <a href="/subscriptions">Subscriptions</a>
+                    <a href="/subscriptions/new">New</a>
                 </div>
             </div>
         {/if}
