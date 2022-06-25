ac29dbb39c2e4af7d0658f53eb2037b8382e8fb9
aylamar <aylamar@users.noreply.github.com>
2022-06-24 22:43:37 -0500

    feat: improved inbox in thumbnail

--- src/client/lib/components/search/thumbnail.svelte
+++ src/client/lib/components/search/thumbnail.svelte
@@ -51,14 +51,13 @@
         <img alt="img" class="rounded-xl w-52 h-52 object-cover shadow-xl" src="{getImgUrl(filename)}">
         <!-- <img alt="img" class="rounded-xl shadow-xl" src="{getImgUrl(filename)}"> -->
     </a>
+
     {#if status === 'inbox'}
-        <div>
-            <span class="absolute top-3 right-3">
-                <svg width="24px" height="24px" viewBox="0 0 128 128" class="{color}"
-                     on:click={updateFileStatus(id, status)}>
-                    <path d="M4.878,104.293h113.125c2.682,0,4.879-2.211,4.879-4.914l-0.115-38.84 c-0.006-2.645-0.387-4.012-1.338-6.492L102.379,4.38C101.516,2.132,100.408,0,97.998,0H25.729c-2.41,0-3.488,2.144-4.38,4.38 L1.22,54.865c-0.966,2.424-1.063,3.809-1.072,6.438L0,99.379C0,102.082,2.198,104.293,4.878,104.293L4.878,104.293z M107.496,57.518H82.111l-7.758,15.617H48.633l-7.862-15.617H15.195l17.922-41.943h57.184L107.496,57.518L107.496,57.518z"></path>
-                </svg>
-            </span>
+        <div class="bg-slate-800/50 w-52 h-6 absolute top-2 rounded-t-xl flex justify-end">
+            <svg width="20px" height="20px" viewBox="0 0 128 128" class="{color} mt-1 mr-1.5"
+                 on:click={updateFileStatus(id, status)}>
+                <path d="M4.878,104.293h113.125c2.682,0,4.879-2.211,4.879-4.914l-0.115-38.84 c-0.006-2.645-0.387-4.012-1.338-6.492L102.379,4.38C101.516,2.132,100.408,0,97.998,0H25.729c-2.41,0-3.488,2.144-4.38,4.38 L1.22,54.865c-0.966,2.424-1.063,3.809-1.072,6.438L0,99.379C0,102.082,2.198,104.293,4.878,104.293L4.878,104.293z M107.496,57.518H82.111l-7.758,15.617H48.633l-7.862-15.617H15.195l17.922-41.943h57.184L107.496,57.518L107.496,57.518z"></path>
+            </svg>
         </div>
     {/if}
 </div>
